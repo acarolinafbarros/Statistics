191e55224102c95c748232ed4aca9e65031aee8e
MafaldaLandeiro <1110590@isep.ipp.pt>
2016-11-27 22:02:59 +0000

    Spring Boot configuration

--- iStat.api/iStat.api/src/main/java/org/iStat/api/iApp/App.java
+++ iStat.api/iStat.api/src/main/java/org/iStat/api/iApp/App.java
@@ -3,7 +3,7 @@ package org.iStat.api.iApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages="org.iStat.api")
 public class App {
 
     public static void main(String[] args) {
