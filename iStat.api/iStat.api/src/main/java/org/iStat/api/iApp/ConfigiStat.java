c35f954b9a3f46847aa87bb67cd5240e7712cb8a
MafaldaLandeiro <1110590@isep.ipp.pt>
2016-11-27 18:04:23 +0000

    Spring configuration

--- iStat.api/iStat.api/src/main/java/org/iStat/api/iApp/ConfigiStat.java
+++ iStat.api/iStat.api/src/main/java/org/iStat/api/iApp/ConfigiStat.java
@@ -1,7 +1,13 @@
 package org.iStat.api.iApp;
 
-// Adicionar as tags
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+// Adicionar as tags
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "org.iStat.api" })
 public class ConfigiStat {
 
 }
