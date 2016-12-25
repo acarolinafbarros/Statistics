ba5610575cdbc5c18d216157b90a73a92521c966
MafaldaLandeiro <miflandeiro@gmail.com>
2016-12-25 23:34:53 +0000

    Draw charts conclusion. ref #10

--- iStat.com/app/controllers/iGraphController.js
+++ iStat.com/app/controllers/iGraphController.js
@@ -1,6 +1,6 @@
 'use strict';
-var valuesChart = '';
 var labelsChart = '';
+var datasetChart = '';
 angular
 		.module('iStatControllers')
 		.controller(
@@ -40,10 +40,8 @@ angular
 									result = getValuesDataset(hot.countCols(),
 											hot.countRows(), null, null);
 								}
-								valuesChart = result[1];
 								labelsChart = result[0];
-								console.log(valuesChart);
-								console.log(labelsChart);
+								datasetChart = result[1];
 								$scope.closeThisDialog();
 								var newScopeChart = $scope;
 								newScopeChart.graphName = $name;
@@ -57,6 +55,10 @@ angular
 									});
 									break;
 								case 'Bar Chart':
+									if (datasetChart.length == 1) {
+										datasetChart[0].backgroundColor = datasetChart[0].backgroundColor[0];
+										datasetChart[0].borderColor = datasetChart[0].borderColor[0];
+									}
 									ngDialog.open({
 										template : 'popUps/popUpBarChart.html',
 										className : 'ngdialog-theme-default',
@@ -64,6 +66,10 @@ angular
 									});
 									break;
 								case 'Line Chart':
+									for (var datasetIndex = 0; datasetIndex < datasetChart.length; datasetIndex++) {
+										datasetChart[datasetIndex].backgroundColor = datasetChart[datasetIndex].backgroundColor[0];
+										datasetChart[datasetIndex].borderColor = datasetChart[datasetIndex].borderColor[0];
+									}
 									ngDialog
 											.open({
 												template : 'popUps/popUpLineChart.html',
@@ -79,23 +85,38 @@ angular
 
 							function getValuesDataset(maxColumn, maxRow,
 									rowIndex, columnIndex) {
+								var datasets = new Array();
+								var serieLabel = new Array();
 								var values = new Array();
 								var labels = new Array();
-								console.log(rowIndex);
+
 								if (rowIndex != null && columnIndex == null) {
-									console.log('rows only');
+									serieLabel.push(hot.getRowHeader(rowIndex));
 									for (var column = 0; column <= maxColumn; column++) {
 										var value = hot.getDataAtCell(rowIndex,
 												column);
 										if (value != null && value != '') {
-											labels.push(hot.getColHeader(column));
+											labels.push(hot
+													.getColHeader(column));
 											values.push(value);
 										}
+									}
+									if (values[0]) {
+										datasets.push({
+											"label" : serieLabel,
+											"data" : values,
+											"backgroundColor" : getColor(
+													values.length, false),
+											"borderColor" : getColor(
+													values.length, false),
+											"borderWidth" : 1
+										});
 									}
 
 								} else if (rowIndex == null
 										&& columnIndex != null) {
-									console.log('columns only');
+									serieLabel.push(hot
+											.getColHeader(columnIndex));
 									for (var row = 0; row <= maxRow; row++) {
 										var value = hot.getDataAtCell(row,
 												columnIndex);
@@ -104,29 +125,98 @@ angular
 											values.push(value);
 										}
 									}
+									if (values[0]) {
+										datasets.push({
+											"label" : serieLabel,
+											"data" : values,
+											"backgroundColor" : getColor(
+													values.length, false),
+											"borderColor" : getColor(
+													values.length, false),
+											"borderWidth" : 1
+										});
+									}
 
 								} else {
-									console.log('dataset');
-									for (var row = 0; row <= maxRow; row++) {
-										for (var column = 0; column <= maxColumn; column++) {
+									var first = true;
+									var labelsAux = new Array();
+									for (var column = 0; column <= maxColumn; column++) {
+										values = new Array();
+										labelsAux = new Array();
+										for (var row = 0; row <= maxRow; row++) {
 											var value = hot.getDataAtCell(row,
 													column);
 											if (value != null && value != '') {
+												if (first) {
+													serieLabel
+															.push(hot
+																	.getColHeader(column));
+													first = false;
+												}
+												if (labels.indexOf(hot
+														.getRowHeader(row)) == -1) {
+													labels.push(hot
+															.getRowHeader(row));
+												}
+												labelsAux.push(hot
+														.getRowHeader(row));
 												values.push(value);
 											}
 										}
+										first = true;
+										var colors = getColor(values.length,
+												true);
+										if (serieLabel[0]) {
+											datasets.push({
+												"label" : serieLabel,
+												"data" : values,
+												"backgroundColor" : colors,
+												"borderColor" : colors,
+												"borderWidth" : 1
+											});
+										}
+										serieLabel = new Array();
 									}
-
 								}
 								var result = new Array();
 								result.push(labels);
-								result.push(values);
+								result.push(datasets);
+								return result;
+							}
+
+							function getColor(size, equalLines) {
+								var result = new Array();
+								if (equalLines) {
+									var rgb = [];
+
+									for (var i = 0; i < 3; i++) {
+										rgb.push(Math
+												.floor(Math.random() * 255));
+									}
+									for (var numberColors = 0; numberColors < size; numberColors++) {
+										result.push('rgba(' + rgb.join(',')
+												+ ', 0.4)');
+									}
+								} else {
+
+									for (var numberColors = 0; numberColors < size; numberColors++) {
+										var rgb = [];
+
+										for (var i = 0; i < 3; i++) {
+											rgb
+													.push(Math.floor(Math
+															.random() * 255));
+										}
+
+										result.push('rgba(' + rgb.join(',')
+												+ ', 0.4)');
+									}
+								}
 								return result;
 							}
 
 							function getColFromName(name) {
 								var n_cols = hot.countCols();
-								console.log(n_cols);
 								var i = 1;
 								for (i = 0; i <= n_cols; i++) {
 									if (name.toLowerCase() == hot.getColHeader(
@@ -139,7 +229,6 @@ angular
 
 							function getLineFromName(name) {
 								var n_rows = hot.countRows();
-								console.log(n_rows);
 								var i = 1;
 								name = name.toString();
 								for (i = 0; i <= n_rows; i++) {
